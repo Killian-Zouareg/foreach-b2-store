8850a26babd35d14f130dc14cc42a341809b4b89
Geraud <geraudmasson@gmail.com>
2021-11-22 19:05:21 +0100

    feat: add lion

---
+++ animals/lion.js
@@ -0,0 +1,13 @@
+var sleep = function () {
+  console.log('zzzzZZZZ');
+};
+
+var eat = function () {
+  console.log('miam');
+};
+
+var roar = function () {
+  console.log('ROOOOOOAAAARRRRR');
+};
+
+module.exports = { eat, sleep, roar };
---
+++ index.js
@@ -0,0 +1,11 @@
+const lion = require('./animals/lion')
+
+function wakeUpTheZoo () {
+    lion.roar()
+}
+
+function giveFood() {
+    lion.miam()
+}
+
+wakeUpTheZoo()
