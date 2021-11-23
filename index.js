647994a51d6149398247cef9e5e67c5e35e08966
Adrien Bogacz <ad.bogacz@gmail.com>
2021-11-23 09:49:20 +0100

    feat: add singe

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const lion = require('./animals/lion');
+const singe = require('./animals/singe');
 
 function wakeUpTheZoo() {
   lion.roar();
+  singe.roar();
 }
 
 function giveFood() {
   lion.miam();
+  singe.eat();
 }
 
 function closeTheZoo() {
   lion.sleep();
+  singe.sleep();
 }
+
+
 
 wakeUpTheZoo();
