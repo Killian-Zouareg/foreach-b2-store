66931c78fa07682c5899bb68d420c990afdd41cc
FvAlex <fievezalexandre.git@gmail.com>
2021-11-23 12:06:03 +0100

    feat: ajout d'animaux

---
+++ animals/cheval.js
@@ -0,0 +1,14 @@
+var sleep = function () {
+    console.log('zzzzZZZZ');
+  };
+  
+  var eat = function () {
+    console.log('miam');
+  };
+  
+  var roar = function () {
+    console.log('HIIIIIIIIIIIIIIIIIIII');
+  };
+  
+  module.exports = { eat, sleep, roar };
+  
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const lion = require('./animals/lion');
+<<<<<<< HEAD
 const loup = require('./animals/loup');
 const mouton = require('./animals/mouton');
 const singe = require('./animals/singe');
@@ -8,20 +9,35 @@ function wakeUpTheZoo() {
   mouton.roar();
   singe.roar();
   loup.howling();
+=======
+const cheval = require('./animals/cheval');
+
+function wakeUpTheZoo() {
+  lion.roar();
+  cheval.roar();
+>>>>>>> feat: ajout du Cheval
 }
 
 function giveFood() {
   lion.miam();
+<<<<<<< HEAD
   mouton.miam();
   singe.eat();
   loup.miam();
+=======
+  cheval.miam();
+>>>>>>> feat: ajout du Cheval
 }
 
 function closeTheZoo() {
   lion.sleep();
+<<<<<<< HEAD
   mouton.sleep();
   singe.sleep();
   loup.sleep();
+=======
+  cheval.sleep();
+>>>>>>> feat: ajout du Cheval
 }
 
 
