f15bf089e2333f455756643d18209c132b96b844
Maxime Wallart <wallart.maxime.pro@gmail.com>
2021-11-23 10:23:16 +0100

    feat: updated the Crotal

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const lion = require('./animals/lion');
 const loup = require('./animals/loup');
 const mouton = require('./animals/mouton');
 const singe = require('./animals/singe');
+const crotal = require('./animals/crotal');
 
 function wakeUpTheZoo() {
   lion.roar();
   mouton.roar();
   singe.roar();
   loup.howling();
+  crotal.rattle();
+  crotal.hissing();
+
 }
 
 function giveFood() {
@@ -15,6 +19,7 @@ function giveFood() {
   mouton.miam();
   singe.eat();
   loup.miam();
+  crotal.eat();
 }
 
 function closeTheZoo() {
@@ -22,6 +27,7 @@ function closeTheZoo() {
   mouton.sleep();
   singe.sleep();
   loup.sleep();
+  crotal.sleep();
 }
 
 
