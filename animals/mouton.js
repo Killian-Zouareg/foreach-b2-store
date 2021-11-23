6fb2acdca0ba4e87b49e864a42ec7699930e2ba2
unknown <isaaclc937@gmail.com>
2021-11-23 09:56:59 +0100

    feat: add animal
    feat #10

---
+++ animals/mouton.js
@@ -0,0 +1,14 @@
+var sleep = function () {
+    console.log('rommmmpchiiiii');
+  };
+  
+  var eat = function () {
+    console.log('miam');
+  };
+  
+  var roar = function () {
+    console.log('BRRRRRrrrrrr');
+  };
+  
+  module.exports = { eat, sleep, roar };
+  
--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const lion = require('./animals/lion');
+const mouton = require('./animals/mouton');
 
 function wakeUpTheZoo() {
   lion.roar();
+  mouton.roar();
 }
 
 function giveFood() {
   lion.miam();
+  mouton.miam();
 }
 
 function closeTheZoo() {
   lion.sleep();
+  mouton.sleep();
 }
 
 wakeUpTheZoo();
