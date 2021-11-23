36267705585df72923f270db17b2b1d944026173
Dussart Tristan <72014679+Tristan-Dussart-fr@users.noreply.github.com>
2021-11-23 13:30:28 +0100

    feat: addTigre (#20)
    * feat: addTigre
    
    feat #10

---
+++ animals/tigre.js
@@ -0,0 +1,13 @@
+var sleep = function () {
+    console.log('zzzzZZZZ');
+  };
+  
+  var eat = function () {
+    console.log('miam');
+  };
+  
+  var roar = function () {
+    console.log('GGGGRRRRRRR');
+  };
+  
+  module.exports = { eat, sleep, roar };
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const lion = require('./animals/lion');
+const tigre = require('./animals/tigre');
 const loup = require('./animals/loup');
 const mouton = require('./animals/mouton');
 const singe = require('./animals/singe');
 
 function wakeUpTheZoo() {
   lion.roar();
+  tigre.roar();
   mouton.roar();
   singe.roar();
   loup.howling();
@@ -12,6 +14,7 @@ function wakeUpTheZoo() {
 
 function giveFood() {
   lion.miam();
+  tigre.miam();
   mouton.miam();
   singe.eat();
   loup.miam();
@@ -19,11 +22,10 @@ function giveFood() {
 
 function closeTheZoo() {
   lion.sleep();
+  tigre.sleep();
   mouton.sleep();
   singe.sleep();
   loup.sleep();
 }
-
-
 
 wakeUpTheZoo();
