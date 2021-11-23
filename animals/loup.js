d707f21af6355522a8e87cf697ebe1d48440057e
Killian-Zouareg <82818206+Killian-Zouareg@users.noreply.github.com>
2021-11-23 11:56:14 +0100

    Feat/add loup (#18)
    * feat: addLoup

---
+++ animals/loup.js
@@ -0,0 +1,14 @@
+var sleep = function () {
+    console.log('zzzzZZZZ');
+  };
+  
+  var eat = function () {
+    console.log('miam');
+  };
+  
+  var howling = function () {
+    console.log('AAAAAHHOOUUUUUUU');
+  };
+  
+  module.exports = { eat, sleep, howling };
+  
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const lion = require('./animals/lion');
+const loup = require('./animals/loup');
 const mouton = require('./animals/mouton');
 const singe = require('./animals/singe');
 
@@ -6,18 +7,21 @@ function wakeUpTheZoo() {
   lion.roar();
   mouton.roar();
   singe.roar();
+  loup.howling();
 }
 
 function giveFood() {
   lion.miam();
   mouton.miam();
   singe.eat();
+  loup.miam();
 }
 
 function closeTheZoo() {
   lion.sleep();
   mouton.sleep();
   singe.sleep();
+  loup.sleep();
 }
 
 
